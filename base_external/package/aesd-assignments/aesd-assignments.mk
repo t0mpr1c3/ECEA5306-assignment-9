5348aaa6229f89ff39cec3ba758ba8d616b5c568
Tom Price <t0mpr1c3@gmail.com>
2025-06-23 00:29:21 -0400

    fix aesd-assignments.mk, Makefile, and kernel config

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f4a6eb44315f0e7605ff65d308636532a0cc48fb'
+AESD_ASSIGNMENTS_VERSION = 'fc54f9db0124b8bde9db88b9f23f0f6111abbe03'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,6 +23,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- build.sh
+++ build.sh
@@ -28,6 +28,6 @@ then
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
+	FORCE_UNSAFE_CONFIGURE=1 make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 
 fi
